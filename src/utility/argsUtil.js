b04b407a02ca84a27a74f53ff373b40a9397362c
spicadev <155805184+spicadev@users.noreply.github.com>
2024-01-07 09:12:32 +0700

    Create argsUtil.js

---
+++ src/utility/argsUtil.js
@@ -0,0 +1,22 @@
+var argsUtil = module.exports = exports = {}
+
+argsUtil.validateObject = function(arg, rules, context) {
+  for(var i = 0; i < rules.length; i++) {
+    var rule = rules[i]
+    if(typeof rule == 'string') {
+      rule = { key: rule }
+    }
+    var value = arg[rule.key]
+    if(!(rule.key in arg)) {
+      if('default' in rule) {
+        arg[rule.key] = rule.default
+      } else {
+        throw new Error('Missing object property `' + rule.key + '` in ' + context)
+      }
+    } else if('accept' in rule && 
+              (typeof rule.accept == 'string' && rule.accept != typeof value) ||
+              (typeof rule.accept == 'functiom' && !(value instanceof rule.accept))) {
+      throw new Error('Property ' + rule.key + ' only accept value as ' + rule.accept + '.')
+    }
+  return arg
+}
