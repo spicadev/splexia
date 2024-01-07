23263554805ac3b2313164cf5a586ae8bcd38adc
spicadev <155805184+spicadev@users.noreply.github.com>
2024-01-07 08:34:33 +0700

    Create abc-lang.js

---
+++ examples/abc-lang.js
@@ -0,0 +1,37 @@
+const splexia = require('splexia')
+const abc = {}
+
+abc.lexer = splexia.lexer.charLoop({
+  onChar: function(char) {
+    if(char === '/' && this.peek(1) === '*') {
+      this.i += 2
+      while(this.get() !== '/' && this.peek(-1) !== '*') {
+        this.i++
+      }
+    } else if(char === '"') {
+      this.i++
+      const str = []
+      while(this.get() !== '"' && this.peek(-1) !== '\\') {
+        str.push(this.get())
+      }
+      this.push(str.join(''))
+    } else if(/\s/.test(char)) { /* ignore: auto-skip */ }
+  },
+  onError: console.error
+})
+
+/* or:
+abc.lexer = splexia.lexer.rulesBased({
+  rules: [
+    { regex: /\/\*[\s\S]*(?:\*\/)|$/g, ignore: true },
+    { regex: /("|').*\1/g, type: 'STRING' },
+    { regex: /\s/, ignore: true }
+  ],
+  onError: console.error
+})
+*/
+
+abc.lexer(`
+/* This is a comment */
+'String Works!'
+`)
